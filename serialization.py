9c9802783e767ce5a4e23a76c56f0d07cf69fc7b
OlekKruh <ksalexks@gmail.com>
2023-12-07 21:10:37 +0100

    Merge remote-tracking branch 'origin/Olek_Instruction_Installation' into Olek_Instruction_Installation
    # Conflicts:
    #	address_book_aleksandra.py

--- serialization.py
+++ serialization.py
@@ -1,1 +1,56 @@
-import pickle
+import json
+from address_book_aleksandra import Record, Phone
+from notebook import Notepade, note
+
+
+# Kule dla wyciągania numerów z listy [...] w string.
+def extract_phone(phone_list):
+    return ', '.join(str(Phone(number)) for number in phone_list)
+
+
+def to_json_book(address_book, filename):
+    data_to_write = {
+        name: {
+            'name': str(record.name),
+            'phone': list(map(str, record.phone)),
+            'emaile': str(record.birthday),
+            'birthday': str(record.birthday) if record.birthday else None
+        } for name, record in address_book.data.items()
+    }
+    with open(filename, 'w') as data_file:
+        json.dump(data_to_write, data_file, indent=2)
+
+
+def from_json_book(filename):
+    with open(filename, 'r') as data_file:
+        data = json.load(data_file)
+        records = {}
+
+        for name, record_data in data.items():
+            phone_list = record_data.get('phone', [])
+            phone_string = extract_phone(phone_list)
+            record = Record(
+                name,
+                phone_string,
+                record_data.get('emaile'),
+                record_data.get('birthday'),
+            )
+            records[name] = record
+    return records
+
+
+def to_json_note(notepad, filename):
+    data_to_write = [
+        {
+            'title': title,
+            'text': note.text
+        } for title, note in notepad.items()
+    ]
+    with open(filename, 'w') as data_file:
+        json.dump(data_to_write, data_file, indent=2)
+
+
+def from_json_note(filename):
+    with open(filename, 'r') as data_file:
+        data = json.load(data_file)
+        return {note['title']: note['text'] for note in data}

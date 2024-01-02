10990ca48f01fbecfa3b7302308d8be66dd33a54
OlekKruh <ksalexks@gmail.com>
2024-01-03 00:43:53 +0100

    Olek_Kruh

--- CLI_AddressBook V0.2/Modules/main.py
+++ CLI_AddressBook V0.2/Modules/main.py
@@ -2,37 +2,37 @@ from notebook import Note, NoteBook
 from serialization import to_json_note, from_json_note
 
 
-# Создаем экземпляр NoteBook
+# Create a NoteBook
 notebook = NoteBook()
 
 
-# Создаем экземпляр Note
+# Create a Note
 note1 = Note("Example Note 1", "This is an example note.")
 note2 = Note("Example Note 2", "This is an example note.")
 
 
-# Добавляем теги к заметке
+# Adding tags to a note
 note1.add_tag("banana")
 note2.add_tag("salami")
 note2.add_tag("apple")
 
 
-# Сохраняем NoteBook в файл JSON
+# Save NoteBook to JSON file
 notebook.add_note(note1)
 notebook.add_note(note2)
 filename = "notebook.json"
 to_json_note(notebook, filename)
 
 
-# Загружаем NoteBook из файла JSON
+# Loading NoteBook from JSON file
 loaded_notebook = from_json_note(filename)
 
 
-# Выводим содержимое загруженного NoteBook
+# Displaying the contents of the downloaded NoteBook
 print(f"Loaded NoteBook:\n{loaded_notebook}")
 
 
-# Поиск заметки по тегу
+# Search for a note by tag
 tag_to_search = "apple"
 matching_notes = loaded_notebook.search_tag(tag_to_search)
 print(f"Notes with tag '{tag_to_search}':\n{matching_notes}")
@@ -42,7 +42,7 @@ matching_notes = loaded_notebook.search_tag(tag_to_search)
 print(f"Notes with tag '{tag_to_search}':\n{matching_notes}")
 
 
-# # Поиск заметки по заголовку
+# Search note by title
 title_to_search = "Example"
 matching_notes = loaded_notebook.search_note(title_to_search)
 print(f"Notes with title '{title_to_search}':\n{matching_notes}")
@@ -52,12 +52,12 @@ matching_notes = loaded_notebook.search_note(title_to_search)
 print(f"Notes with title '{title_to_search}':\n{matching_notes}")
 
 
-# Сортировка по тагу
+# Sorting by tag
 note_sorted_by_tag = loaded_notebook.sort_tag()
 print(f"Sorted by Tag:\n{note_sorted_by_tag}")
 
 
-# Удаление заметки
+# Deleting a note
 note_to_delete = "Example Note 3"
 loaded_notebook.delete_note(note_to_delete)
 
@@ -65,5 +65,5 @@ note_to_delete = "Example Note 1"
 loaded_notebook.delete_note(note_to_delete)
 
 
-# Выводим обновленное содержимое NoteBook
+# Displaying updated NoteBook contents
 print(f"Updated NoteBook:\n{loaded_notebook}")

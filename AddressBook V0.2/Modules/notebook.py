163447355169190f291dcff19ca8e3201ab8732e
OlekKruh <ksalexks@gmail.com>
2024-01-03 00:48:12 +0100

    Olek_Kruh

--- AddressBook V0.2/Modules/notebook.py
+++ AddressBook V0.2/Modules/notebook.py
@@ -1,32 +1,32 @@
-import re
 from collections import UserDict
-import curses
 
 
 class Note:
     def __init__(self, title, text=None, tags=None):
-        self._title = title
-        self._tags = tags or []
-        self._text = text
+        self.title = title
+        self.tags = tags or []
+        self.text = text
 
     def add_tag(self, tag):
-        self._tags.append(tag)
+        self.tags.append(tag)
 
     def delete_tag(self, tag):
-        self._tags = [i for i in self._tags if str(i) != tag]
-        if not self._tags:
-            print(f'All tags have been deleted.')
+        self.tags = [i for i in self.tags if str(i) != tag]
+        if not self.tags:
+            print(f"All tags have been deleted.")
 
     def rewrite_note(self, new_title=None, new_text=None):
         if new_title is not None:
-            self._title = new_title
+            self.title = new_title
         if new_text is not None:
-            self._text = new_text
+            self.text = new_text
 
     def __str__(self):
-        return (f'Title: {self._title}\n'
-                f'Tag: {", ".join(map(str, self._tags))}\n'
-                f'Text: {self._text if self._text else ""}\n')
+        return (
+            f"Title: {self.title}\n"
+            f'Tag: {", ".join(map(str, self.tags))}\n'
+            f'Text: {self.text if self.text else ""}\n'
+        )
 
 
 class NoteBook(UserDict):
@@ -35,7 +35,9 @@ class NoteBook(UserDict):
 
     def search_tag(self, tag):
         matching_records = [
-            record for record in self.data.values() if any(tag.lower() in t.lower() for t in record._tags)
+            record
+            for record in self.data.values()
+            if any(tag.lower() in t.lower() for t in record.tags)
         ]
         if not matching_records:
             return "No records to display.\n"
@@ -44,7 +46,7 @@ class NoteBook(UserDict):
 
     def search_note(self, title):
         matching_records = [
-            note for note in self.data.values() if title.lower() in note._title.lower()
+            note for note in self.data.values() if title.lower() in note.title.lower()
         ]
         if not matching_records:
             return "No records to display.\n"
@@ -52,16 +54,16 @@ class NoteBook(UserDict):
 
     def sort_tag(self):
         for records in self.data.values():
-            records._tags.sort()
+            records.tags.sort()
 
-        sorted_records = sorted(self.data.values(), key=lambda record: record._tags)
+        sorted_records = sorted(self.data.values(), key=lambda record: record.tags)
         return self.__str__(sorted_records)
 
     def add_note(self, record: Note):
-        if record._title is None:
-            raise ValueError(f'Error: Note cannot be created.\n')
+        if record.title is None:
+            raise ValueError(f"Error: Note cannot be created.\n")
         else:
-            self.data[record._title] = record
+            self.data[record.title] = record
 
     def delete_note(self, title):
         if title in self.data:

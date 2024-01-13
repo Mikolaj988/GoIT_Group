ac46c6db709ab45ec9cccce23b7ed1bfcddf91b9
OlekKruh <ksalexks@gmail.com>
2024-01-13 23:23:44 +0100

    Olek_Kruh

--- AddressBookV0.2/Modules/addressbook.py
+++ AddressBookV0.2/Modules/addressbook.py
@@ -7,15 +7,16 @@ class Contact:
     def __init__(self, name, phone=None, birthday=None, email=None):
         try:
             if not Contact.validation_name(name):
-                raise ValueError(f'Error: Invalid Name format for {name}')
+                raise ValueError(f'Error: INVALID Name format for {name}')
         except ValueError as e:
             print(e)
             self.name = None
         else:
             self.name = name
-            self.phone = phone or []
+            self.phone = [phone] or []
             self.birthday = birthday
             self.email = email
+            print(f'Contact {self.name} was CREATED.\n')
 
     @staticmethod
     def validation_name(name):
@@ -46,63 +47,124 @@ class Contact:
     def add_phone(self, phone):
         try:
             if not Contact.validation_phone(phone):
-                raise ValueError(f'Error: Invalid phone number format for {phone}\n')
+                raise ValueError(f'Error: INVALID phone number format for {phone}\n')
         except ValueError as e:
             print(e)
         else:
             self.phone.append(phone)
+            print(f'Phone {phone} was ADD to contact {self.name}\n')
 
     def delete_phone(self, phone):
         self.phone = [i for i in self.phone if str(i) != phone]
+        print(f'Phone {phone} was DELETE from contact {self.name}\n')
 
     def edit_phone(self, old_phone, new_phone):
         try:
             if not Contact.validation_phone(new_phone):
-                raise ValueError(f'Error: Invalid phone number format for {new_phone}\n')
+                raise ValueError(f'Error: INVALID phone number format for {new_phone}\n')
         except ValueError as e:
             print(e)
         else:
             for key, elem in enumerate(self.phone):
                 if str(elem) == old_phone:
                     self.phone[key] = new_phone
+                    print(f'Phone number "{old_phone}" has been successfully CHANGED to "{new_phone}"\n')
                     break
 
     def add_birthday(self, birthday):
         try:
             if not Contact.validation_birthday(birthday):
-                raise ValueError(f'Error: Invalid birthday format for {birthday}\n')
+                raise ValueError(f'Error: INVALID birthday format for {birthday}\n')
         except ValueError as e:
             print(e)
         else:
-            self.birthday = birthday
+            if self.birthday:
+                print(f'Contact already haw birth date\n')
+            else:
+                self.birthday = birthday
+                print(f'Birthday {birthday} has been successfully ADDED to contact {self.name}\n')
 
     def delete_birthday(self, birthday):
-        pass
+        try:
+            if not Contact.validation_birthday(birthday):
+                raise ValueError(f'INVALID birthday format for {birthday}\n')
+        except ValueError as e:
+            print(e)
+        else:
+            if self.birthday == birthday:
+                self.birthday = None
+                print(f'Birthday {birthday} was DELETED from contact {self.name}\n')
+            else:
+                print(f'Error: Provided birthday does NOT MATCH the contact\'s birthday\n')
 
-    def edit_birthday(self, old_birthday, new_birthday):
-        pass
+    def edit_birthday(self, new_birthday):
+        try:
+            if not Contact.validation_birthday(new_birthday):
+                raise ValueError(f'Error: INVALID birthday format for {new_birthday}\n')
+        except ValueError as e:
+            print(e)
+        else:
+            old_birthday = self.birthday
+            self.birthday = new_birthday
+            print(f'Birthday "{old_birthday}" has been successfully CHANGED to "{new_birthday}"\n')
 
     def add_email(self, email):
         try:
             if not Contact.validation_email(email):
-                raise ValueError(f'Error: Invalid email format for {email}\n')
+                raise ValueError(f'Error: INVALID email format for {email}\n')
         except ValueError as e:
             print(e)
         else:
-            self.email = email
+            if self.email:
+                print(f'Contact already haw Email\n')
+            else:
+                self.email = email
+                print(f'Email {email} has been successfully ADDED to contact {self.name}\n')
 
     def delete_email(self, email):
-        pass
+        try:
+            if not Contact.validation_email(email):
+                raise ValueError(f'INVALID email format for {email}\n')
+        except ValueError as e:
+            print(e)
+        else:
+            if self.email == email:
+                self.email = None
+                print(f'Email {email} was DELETED from contact {self.name}\n')
+            else:
+                print(f'Error: Provided Email does NOT MATCH the contact\'s Email\n')
 
-    def edit_email(self, old_email, new_email):
-        pass
+    def edit_email(self, new_email):
+        try:
+            if not Contact.validation_email(new_email):
+                raise ValueError(f'Error: INVALID birthday format for {new_email}\n')
+        except ValueError as e:
+            print(e)
+        else:
+            old_email = self.email
+            self.email = new_email
+            print(f'Birthday "{old_email}" has been successfully CHANGED to "{new_email}"\n')
 
+    def days_to_birthday(self):
+        if not self.birthday:
+            return None
+        else:
+            today = datetime.now().date()
+            day, month, year = map(int, re.findall(r'\d+', str(self.birthday)))
+            next_birthday = datetime(today.year, month, day).date()
+
+            if today > next_birthday:
+                next_birthday = datetime(today.year + 1, month, day).date()
+
+            return (next_birthday - today).days
 
     def __str__(self):
         return (f'Name: {self.name}\n'
                 f'Phone: {", ".join(map(str, self.phone))}\n'
                 f'Birthday: {self.birthday}\n'
-                f'Days to next Birthday left: {''}\n')
+                f'Email: {self.email}\n'
+                f'Days to next Birthday left: {self.days_to_birthday()}\n')
+
 
 class AddressBook:
     pass
--- AddressBookV0.2/Modules/main.py
+++ AddressBookV0.2/Modules/main.py
@@ -1,69 +1,118 @@
+from addressbook import Contact, AddressBook
 from notebook import Note, NoteBook
 from serialization import to_json_note, from_json_note
 
+# NoteBook
+# # Create a NoteBook
+# notebook = NoteBook()
+#
+#
+# # Create a Note
+# note1 = Note("Example Note 1", "This is an example note.")
+# note2 = Note("Example Note 2", "This is an example note.")
+#
+#
+# # Adding tags to a note
+# note1.add_tag("banana")
+# note2.add_tag("salami")
+# note2.add_tag("apple")
+#
+#
+# # Save NoteBook to JSON file
+# notebook.add_note(note1)
+# notebook.add_note(note2)
+# filename = "notebook.json"
+# to_json_note(notebook, filename)
+#
+#
+# # Loading NoteBook from JSON file
+# loaded_notebook = from_json_note(filename)
+#
+#
+# # Displaying the contents of the downloaded NoteBook
+# print(f"Loaded NoteBook:\n{loaded_notebook}")
+#
+#
+# # Search for a note by tag
+# tag_to_search = "apple"
+# matching_notes = loaded_notebook.search_tag(tag_to_search)
+# print(f"Notes with tag '{tag_to_search}':\n{matching_notes}")
+#
+# tag_to_search = "banana"
+# matching_notes = loaded_notebook.search_tag(tag_to_search)
+# print(f"Notes with tag '{tag_to_search}':\n{matching_notes}")
+#
+#
+# # Search note by title
+# title_to_search = "Example"
+# matching_notes = loaded_notebook.search_note(title_to_search)
+# print(f"Notes with title '{title_to_search}':\n{matching_notes}")
+#
+# title_to_search = "Notes"
+# matching_notes = loaded_notebook.search_note(title_to_search)
+# print(f"Notes with title '{title_to_search}':\n{matching_notes}")
+#
+#
+# # Sorting by tag
+# note_sorted_by_tag = loaded_notebook.sort_tag()
+# print(f"Sorted by Tag:\n{note_sorted_by_tag}")
+#
+#
+# # Deleting a note
+# note_to_delete = "Example Note 3"
+# loaded_notebook.delete_note(note_to_delete)
+#
+# note_to_delete = "Example Note 1"
+# loaded_notebook.delete_note(note_to_delete)
+#
+#
+# # Displaying updated NoteBook contents
+# print(f"Updated NoteBook:\n{loaded_notebook}")
 
-# Create a NoteBook
-notebook = NoteBook()
+# AddressBook
+# Create a Contact
+contact1 = Contact("John Doe")
+print(contact1)
 
+contact1.add_birthday("12.01.1990")
+print(contact1)
 
-# Create a Note
-note1 = Note("Example Note 1", "This is an example note.")
-note2 = Note("Example Note 2", "This is an example note.")
-
-
-# Adding tags to a note
-note1.add_tag("banana")
-note2.add_tag("salami")
-note2.add_tag("apple")
-
-
-# Save NoteBook to JSON file
-notebook.add_note(note1)
-notebook.add_note(note2)
-filename = "notebook.json"
-to_json_note(notebook, filename)
-
-
-# Loading NoteBook from JSON file
-loaded_notebook = from_json_note(filename)
-
-
-# Displaying the contents of the downloaded NoteBook
-print(f"Loaded NoteBook:\n{loaded_notebook}")
-
-
-# Search for a note by tag
-tag_to_search = "apple"
-matching_notes = loaded_notebook.search_tag(tag_to_search)
-print(f"Notes with tag '{tag_to_search}':\n{matching_notes}")
-
-tag_to_search = "banana"
-matching_notes = loaded_notebook.search_tag(tag_to_search)
-print(f"Notes with tag '{tag_to_search}':\n{matching_notes}")
-
-
-# Search note by title
-title_to_search = "Example"
-matching_notes = loaded_notebook.search_note(title_to_search)
-print(f"Notes with title '{title_to_search}':\n{matching_notes}")
-
-title_to_search = "Notes"
-matching_notes = loaded_notebook.search_note(title_to_search)
-print(f"Notes with title '{title_to_search}':\n{matching_notes}")
-
-
-# Sorting by tag
-note_sorted_by_tag = loaded_notebook.sort_tag()
-print(f"Sorted by Tag:\n{note_sorted_by_tag}")
-
-
-# Deleting a note
-note_to_delete = "Example Note 3"
-loaded_notebook.delete_note(note_to_delete)
-
-note_to_delete = "Example Note 1"
-loaded_notebook.delete_note(note_to_delete)
-
-
-# Displaying updated NoteBook contents
-print(f"Updated NoteBook:\n{loaded_notebook}")
+# # Add phone number
+# contact1.add_phone("9876543210")
+# print(contact1)  # ['1234567890', '9876543210']
+#
+# # Delete phone number
+# contact1.delete_phone("1234567890")
+# print(contact1)  # ['9876543210']
+#
+# # Edit phone number
+# contact1.edit_phone("9876543210", "5555555555")
+# print(contact1)  # ['5555555555']
+#
+# # Add more birthday
+# contact1.add_birthday("01.01.1990")
+# # Contact already haw birth date
+#
+# # Edite birthday
+# contact1.edit_birthday("03.03.1980")
+#
+# # Wrong birthday edition
+# contact1.edit_birthday("04.31.1975")
+# print(contact1)
+#
+# # Delete Birthday
+# contact1.delete_birthday("04.10.1975")
+# contact1.delete_birthday("01.01.1990")
+#
+# # Add more email
+# contact1.add_email("john2@example.com")
+# # Contact already haw Email
+# print(contact1)
+#
+# # Edite email
+# contact1.edit_email("john.doe@example.com")
+#
+# # Delete email
+# contact1.delete_email("john@example.com")
+# contact1.delete_email("john.doe@example.com")
+# print(contact1)
